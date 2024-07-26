b7bc958aab2bcb42afc5d770673138c927b9d0a6
Mohamed Karem <m.karem456@gmail.com>
2024-07-27 01:26:01 +0300

    hot fix

---
+++ database/migrations/2024_06_27_212046_update_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Drop the unique constraint
+            $table->dropUnique(['email','phone']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Add the unique constraint back
+            $table->unique('email');
+            $table->unique('phone');
+        });
+    }
+}
